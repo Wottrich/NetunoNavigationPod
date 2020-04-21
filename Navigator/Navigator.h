6aa3f15c23903ae1d5e23b6bae83044ff0720f62
wottrich <lucas.wottrich@operacao.rcadigital.com.br>
2020-04-21 14:54:36 -0300

    Init Navigator project

---
+++ Navigator/Navigator.h
@@ -0,0 +1,19 @@
+//
+//  Navigator.h
+//  Navigator
+//
+//  Created by Wottrich on 21/04/20.
+//  Copyright © 2020 Wottrich Technology Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Navigator.
+FOUNDATION_EXPORT double NavigatorVersionNumber;
+
+//! Project version string for Navigator.
+FOUNDATION_EXPORT const unsigned char NavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Navigator/PublicHeader.h>
+
+
---
+++ NavigatorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigatorExample
+//
+//  Created by Wottrich on 21/04/20.
+//  Copyright © 2020 Wottrich Technology Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
